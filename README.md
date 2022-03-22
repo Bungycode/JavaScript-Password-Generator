88f32c4f9486abbb3ffcc3572df8830263b44903
Andrew Bungcayao <thedevandrew@gmail.com>
2022-03-22 13:41:35 -0400

    Updated javascript code for final product

---
+++ README.md
@@ -0,0 +1,26 @@
+# 03-JavaScript-Password-Generator
+
+## Description
+
+The purpose of this game is to simulate how to randomly create a password for a user after asking a few questions. The program will ask how long the user wants their password to be within a certain numeric range. The user will proceed through several prompts asking the different types of characters they want included in their password. This program will then use a mathematical formula to generate a randomized password and print it in the text box. This password generator is programmed to ensure that the user follows the proper design in creating a correct password. This program uses if statements, for loops, and for...of loops.
+
+## Mock-Up
+
+The following image shows a demonstration of what the finished web application's presentation and functionality will include:
+
+
+![The Password-Generator web page includes generate password container with a text box for the password and a button to start the program.](./assets/images/Password-Generator-Image.png)
+
+## Prerequisites
+
+Before you continue, make sure you have the following requirements:
+
+- You have Visual Studio Code downloaded.
+- You are using a MAC OS or Windows machine.
+- You have a basic understanding of HTML and CSS.
+
+## Links
+
+[This is a URL link to the deployed application.](https://bungycode.github.io/JavaScript-Password-Generator/)
+
+[This is a URL link to the github repository](https://github.com/Bungycode/JavaScript-Password-Generator)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,46 @@
-  // Assignment Code
+  // Assignment Code. Copy and Pasted below at the bottom
   var generateBtn = document.querySelector("#generate");
   
    // Created a function to for the options the user will be able to choose from to design their generated password.
    // Added console.logs to check the progression of the password creation
   function options() {
-    var choices = {};
-    var lowerCasedOption = window.confirm("Do you want lowercased letters in your generated password?")
+      var choices = {};
+      var lowerCasedOption = window.confirm("Do you want lowercased letters in your generated password?")
       if (lowerCasedOption) {
-        choices.lower = lowerCasedOption;
-        console.log(choices);
+          choices.lower = lowerCasedOption;
+          console.log(choices);
       } else {
-        console.log("Password will not contain any lowercased letters!")
+          console.log("Password will not contain any lowercased letters!")
       }
     var upperCasedOption = window.confirm("Do you want uppercased letters in your generated password?")
       if (upperCasedOption) {
-        choices.upper = upperCasedOption;
-        console.log(choices);
+          choices.upper = upperCasedOption;
+          console.log(choices);
       } else {
-        console.log("Password will not contain any uppercased letters!")
+          console.log("Password will not contain any uppercased letters!")
       }
     var numericalOption = window.confirm("Do you want numbers in your generated password?")
       if (numericalOption) {
-        choices.numbers = numericalOption;
-        console.log(choices);
+          choices.numbers = numericalOption;
+          console.log(choices);
       } else {
-        console.log("Password will not contain any numbers!")
+          console.log("Password will not contain any numbers!")
       }
     var specialCharactersOption = window.confirm("Do you want special characters in your generated password?")
       if (specialCharactersOption) {
-        choices.special = specialCharactersOption;
-        console.log(choices);
+          choices.special = specialCharactersOption;
+          console.log(choices);
       } else {
-        console.log("Password will not contain any special characters!")
+          console.log("Password will not contain any special characters!")
       }
-    // Created an object to hold all the keywords with the value's to the answer of the questions. This object will be used for creating the final password.
-    
+
     // If user does not agree to any of the above choices, then it will call the function again until doing so.
     if ((!lowerCasedOption) && (!upperCasedOption) && (!numericalOption) && (!specialCharactersOption)) {
-        alert("You must choose OK to one of the options to generate a random password");
-        options();
+        console.log("Not picking a choice will cause the program to end")
+        return;
     }
     return choices;
+
   }
 
   // Declared local variables for characters and password progression.
@@ -70,42 +70,49 @@
     // Create a variable and assign it the value of options() function call
     var chosenOptions = options();
 
+    // Program will end if the user did not select a type of character to be used in the password
+    if (!chosenOptions) {
+      console.log("Program ending because none of the options were selected");  
+        alert("You must choose OK to one of the options to generate a random password... ending program.");
+        return;
+    }
+
     // Succession if statements to execute a for...of statement if the condition is met and loop through every value of the iterable object and then add them to the creatingPassword variable through the .push method with i as its value.
     if (chosenOptions.lower) {
         for (var i of lowerCased) {
-          creatingPassword.push(i);
-          console.log(creatingPassword);
+            creatingPassword.push(i);
+            console.log(creatingPassword);
         }
     }
 
     if (chosenOptions.upper) {
-      for (var i of upperCased) {
-        creatingPassword.push(i);
-      }
+        for (var i of upperCased) {
+            creatingPassword.push(i);
+        }
     }
 
     if (chosenOptions.numbers) {
-      for (var i of numerical) {
-        creatingPassword.push(i);
-      }
+        for (var i of numerical) {
+            creatingPassword.push(i);
+        }
     }
 
     if (chosenOptions.special) {
-      for (var i of specialCharacters) {
-        creatingPassword.push(i);
-      }
+        for (var i of specialCharacters) {
+            creatingPassword.push(i);
+        }
     }
     chosenOptions['length'] = passwordLength;
     console.log(creatingPassword);
 
-
+    // Used variable passwordLength as the condition that needs to be met in order to get the correct number of characters in the password
     for (var i = 0; i < passwordLength; i++) {
-    console.log(creatingPassword);
-    finalPassword += creatingPassword[Math.floor(Math.random() * creatingPassword.length)];
-    // return finalPassword;    I previously put return right here and it would return my value after one iteration. So make sure you dont do that!
-  }
-  console.log(finalPassword)
-  return finalPassword;
+        console.log(creatingPassword);
+        finalPassword += creatingPassword[Math.floor(Math.random() * creatingPassword.length)];
+        // return finalPassword;    I previously put return right here and it would return my value after one iteration. So make sure you dont do that!
+    }
+    console.log(finalPassword)
+    return finalPassword;
 }
 
 
@@ -114,15 +121,20 @@
     function writePassword() {
     var password = generatePassword();
     console.log(password);
+
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
     console.log(passwordText);
 }
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
 
+
+
+// below is different approaches I was going to use for creating this password generator but decided not to use them.
+// The original code is also listed below in case I want to create a different set of code for this game.
 
 
 
